bdc3760749e4a437a5146ddf76fb9a0cad43f500
結城 黎 <106291652+YukiR0708@users.noreply.github.com>
2023-12-19 12:09:54 +0900

    [Add]Player移動・ジャンプ

---
+++ Assets/Member/Yuki/PlayerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D _rb2d = default;
+    [SerializeField, Tooltip("ˆÚ“®‘¬“x")] float _speed = 0f;
+    float _horizontalInput = default;
+
+    private void Awake()
+    {
+         _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        _rb2d.velocity = new Vector2(_horizontalInput * _speed, _rb2d.velocity.y);
+    }
+}
