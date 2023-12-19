540854d9e53b4716a87ac95ebb5095e7a30937f7
結城 黎 <106291652+YukiR0708@users.noreply.github.com>
2023-12-19 12:34:37 +0900

    [Update]PlayerのAnimationController作成 [Add]Playerの死亡時の処理作成

--- Assets/Member/Yuki/Script/PlayerJump.cs
+++ Assets/Member/Yuki/Script/PlayerJump.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlTypes;
 using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
@@ -7,10 +8,12 @@ public class PlayerJump : MonoBehaviour
     [SerializeField] float _jumpForce = default;
     [SerializeField] int _jumpCount = 0;
     int _jumpCountMax = 2;
+    Animator _playerAnimator = default;
 
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -24,11 +27,17 @@ public class PlayerJump : MonoBehaviour
         {
             _rb2d.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
             _jumpCount++;
+            _playerAnimator.SetBool("isJump", true);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground") _jumpCount = 0;
+        if (collision.gameObject.tag == "Ground")
+        {
+            _jumpCount = 0;
+            _playerAnimator.SetBool(_jumpCount, false);
+        }
+
     }
 }
