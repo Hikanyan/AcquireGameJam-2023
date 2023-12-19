b00b80c7b8a98942350c197c96d9d89206d40377
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-12-19 16:10:38 +0900

    [Update]

--- Assets/Member/Hikanyan/Script/Audio/CriPlay.cs
+++ Assets/Member/Hikanyan/Script/Audio/CriPlay.cs
@@ -7,7 +7,6 @@ public class CriPlay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //CRIAudioManager.Instance.CriBgmPlay(0);
         CRIAudioManager.Instance.CriBgmPlay(0);
         Debug.Log("Play");
     }
--- Assets/Member/Hikanyan/Script/GameManager.cs
+++ Assets/Member/Hikanyan/Script/GameManager.cs
@@ -1,67 +1,114 @@
-﻿
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; // シーン管理のために必要
 
-public class GameManager: AbstractSingleton<GameManager>
+public class GameManager : AbstractSingleton<GameManager>
 {
-    [Tooltip("現在のゲームステート")]
-    [SerializeField] private GameState NowGameState = GameState.None;
-    [Tooltip("InGameから遷移するシーンの名前を設定")]
-    [SerializeField] private string _InGameTOresult = "Result";
-    
-    [Tooltip("ゲーム開始判定（ゲーム中の時はTrue）")]
-    public bool isGame = false;
-    [Tooltip("ゲームクリア判定（クリア時にTrue）")]
-    public bool isClear = false;
-    [Tooltip("制限時間")]
-    [SerializeField] private float _time = 180;
-    [Tooltip("実際の計算に用いるタイマー変数")]
-    private float _timeValue;
-    [Tooltip("制限時間を入れるText")]
-    [SerializeField] private Text _timeText;
-    
-    [SerializeField] GameObject _resultUI;
-    
-    
-    //CriAudioManager _criAudioManager;
-    public void Initialize()
+    [Tooltip("現在のゲームステート")] [SerializeField]
+    private GameState NowGameState = GameState.None;
+
+    [Tooltip("InGameから遷移するシーンの名前を設定")] [SerializeField]
+    private string _InGameToResult = "Result";
+
+    [Tooltip("ゲーム開始判定（ゲーム中の時はTrue）")] public bool isGame = false;
+    [Tooltip("ゲームクリア判定（クリア時にTrue）")] public bool isClear = false;
+    [Tooltip("制限時間")] [SerializeField] private float _time = 180;
+    [Tooltip("実際の計算に用いるタイマー変数")] private float _timeValue;
+
+    [Tooltip("制限時間を入れるText")] [SerializeField]
+    private Text _timeText;
+
+    [SerializeField] private GameObject _resultUI;
+
+    CRIAudioManager _criAudioManager;
+
+    public void Awake()
     {
-        //_criAudioManager = CriAudioManager.Instance;
+        _criAudioManager = CRIAudioManager.Instance;
+        _timeValue = _time;
+        UpdateTimeText();
     }
 
-    public void Title()
+    private void Update()
     {
-        //_criAudioManager.PlayBGM(CriAudioManager.CueSheet.Bgm, "");
+        StateChange();
+    }
+
+
+    private void StateChange()
+    {
+        switch (NowGameState)
+        {
+            case GameState.Start:
+                StartGame();
+                break;
+            case GameState.Result:
+                Result();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void UpdateGameTimer()
+    {
+        if (_timeValue > 0)
+        {
+            _timeValue -= Time.deltaTime;
+            UpdateTimeText();
+        }
+        else
+        {
+            EndGame();
+        }
+    }
+
+    private void UpdateTimeText()
+    {
+        _timeText.text = Mathf.Ceil(_timeValue).ToString();
     }
 
     public void StartGame()
     {
-        //タイマースタート
-        //BGM
-        //
+        isGame = true;
+        NowGameState = GameState.InGame;
+        // タイマースタート
+        if (isGame)
+        {
+            UpdateGameTimer();
+        }
+        // BGM再生など
+        _criAudioManager.CriBgmPlay(0);
     }
+
+
     public void EndGame()
     {
-        //タイマーストップ
-        //BGMストップ
-        //リザルト
-        
+        isGame = false;
+        NowGameState = GameState.Result;
+        // タイマーストップ
+        // BGMストップ
+        // リザルトUIを表示
+        _resultUI.SetActive(true);
+        SceneManager.LoadScene(_InGameToResult);
     }
+
     public void PauseGame()
     {
-        //タイマーストップ 保持
-        //BGMストップ
-        //ポーズ画面
-        Result();
+        isGame = false;
+        // タイマー保持
+        // BGMストップ
+        // ポーズ画面表示など
     }
-    
+
     public void Result()
     {
-        //Score表示 評価
-        //BGM
-        //リザルト画面
-        
+        // スコア表示
+        // BGM再生
+        // リザルト画面表示など
+        _criAudioManager.CriBgmStop();
+        _resultUI.SetActive(true);
     }
 }
 
@@ -71,4 +118,14 @@ internal enum GameState
     Start,
     InGame,
     Result,
+}
+
+public enum ResultRank
+{
+    None = 0,
+    S,
+    A,
+    B,
+    C,
+    D,
 }
