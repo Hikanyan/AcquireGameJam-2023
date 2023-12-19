4a829769ee723b9e8ad4c4dd264f31ab463d7cdb
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-12-19 12:11:24 +0900

    [Add]Boot

---
+++ Assets/Member/Hikanyan/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿
+public class GameManager: AbstractSingleton<GameManager>
+{
+    
+    public void Initialize()
+    {
+        
+    }
+    
+    public void StartGame()
+    {
+        
+    }
+    public void EndGame()
+    {
+        
+    }
+    public void PauseGame()
+    {
+        
+    }
+    
+    public void Result()
+    {
+        
+    }
+}
