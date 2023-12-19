b55491bdd9bd2640afdf74458f14bed6eeb3753f
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2023-12-19 17:46:42 +0900

    スタートとゴールの追加(実際には実験していない)

---
+++ Assets/Member/tejima/Script/start.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    [SerializeField] GameObject wood = null;
+    GameObject player = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(player,wood.transform);
+    }
+}
